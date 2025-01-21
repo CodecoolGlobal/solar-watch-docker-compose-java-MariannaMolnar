9df0289afaefc6c6d31b6185e947554aa6fddf08
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-01-21 20:59:48 +0100

    implementing date range option in SolarDataform

--- frontend/src/features/main/components/organisms/SolarDataForm.jsx
+++ frontend/src/features/main/components/organisms/SolarDataForm.jsx
@@ -4,8 +4,8 @@ import InputField from "../atoms/InputField";
 
 import Button from "../atoms/Button";
 
-function SolarDataForm({ city, setCity, country, setCountry, date, setDate, handleDataRequest }) {
-
+function SolarDataForm({ city, setCity, country, setCountry, date, setDate, endDate, setEndDate, handleDataRequest }) {
+    const [useRange, setUseRange] = useState(false);
 
 
     //const navigate = useNavigate();
@@ -29,12 +29,45 @@ function SolarDataForm({ city, setCity, country, setCountry, date, setDate, hand
                         onChange={(e) => setCountry(e.target.value)}
                         required
                     />
-                    <InputField
-                        label="Date"
-                        type="date"
-                        value={date}
-                        onChange={(e) => setDate(e.target.value)}
-                    />
+                    
+                    <div className="mb-4 max-w-lg flex flex-col">
+                        <label className="block  font-medium mb-2 text-left"> Date </label>
+                        <div className="flex gap-2">
+                            <input
+                                type="checkbox"
+                                id="useRange"
+                                checked={useRange}
+                                onChange={(e) => setUseRange(e.target.checked)}
+                                className="h-4 w-4 accent-customBlue border-gray-300 rounded focus:ring-customBlue mt-1"
+                            />
+                            <label htmlFor="useRange" className="flex gap-2"> Use range </label>
+                        </div>
+                        {useRange ? (
+                            <>
+                                <InputField
+                                    label="From"
+                                    type="date"
+                                    value={date}
+                                    onChange={(e) => setDate(e.target.value)}
+                                />
+                                <InputField
+                                    label="To"
+                                    type="date"
+                                    value={endDate}
+                                    onChange={(e) => setEndDate(e.target.value)}
+                                />
+                            </>
+
+                        ) : (
+                            <InputField
+                                label=" "
+                                type="date"
+                                value={date}
+                                onChange={(e) => setDate(e.target.value)}
+                            />
+                        )}
+
+                    </div>
 
                     <Button type="submit"
                         className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">

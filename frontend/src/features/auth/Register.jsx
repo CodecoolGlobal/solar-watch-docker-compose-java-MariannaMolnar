7bd9d8a6a6c7b366b451ae5daf80cdf1b2b450e4
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2025-01-23 10:11:12 +0100

    changing register logo

--- frontend/src/features/auth/Register.jsx
+++ frontend/src/features/auth/Register.jsx
@@ -1,7 +1,8 @@
 import {useState} from "react";
 import {Link} from "react-router-dom";
-import SolarWatchLogo from "../main/components/atoms/SolarWatchLogo";
 import Button from "../main/components/atoms/Button.jsx";
+import registerLogo from "../../assets/register4.3.png";
+import Logo from "../main/components/atoms/Logo";
 
 function Register() {
     
@@ -49,7 +50,7 @@ function Register() {
         <div>
             <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <SolarWatchLogo h={20}/>
+                    <Logo h={20} logoSrc={registerLogo}/>
                     <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight">Register an
                             account</h2>
 
